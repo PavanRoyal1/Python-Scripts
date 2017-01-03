43113eb0459a839b350bc7a4bf4764777f20183d
PavanRoyal1 <pavankumarp743@gmail.com>
2017-01-02 21:35:06 -0500

    deleting all files

--- cookbooks/sendingreports/attributes/patch.rb
+++
@@ -1,7 +0,0 @@
-default['patching']['user'] = 'user'
-default['patching']['ip'] = '54.85.35.164'
-default['patching']['sourcefile'] = "/tmp/silicus/reports/#{node.name}.csv"
-default['patching']['destinationpath'] = "/tmp/#{node.name}.csv"
-default['patching']['password'] = '123456'
-default['patching']['scrpt_path'] = '/tmp'
-default['patching']['scrpt_file'] = 'filecpy.sh'
